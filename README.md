22f5191d717f90a26105e08f42135e03a9ea9f63
alexjackhughes <info@alexjackhughes.com>
2019-08-28 11:41:48 +0100

    Added readme docs and skelton api

--- README.md
+++ README.md
@@ -15,6 +15,44 @@ npm start
 The **Backend API** will be available on:
 [localhost:5000](http://localhost:5000/ "http://localhost:5000/")
 
+## Documentation
+
+**GET**
+
+```
+GET /youtube
+```
+
+_Fetch any YouTube videos meta data that has been stored._
+
+```
+GET /youtube/:id
+```
+
+_Fetch any YouTube video by it's ID._
+
+```
+GET /youtube/search/:search
+```
+
+_Fetch all videos that match the provided search query._
+
+**POST**
+
+```
+POST /youtube
+```
+
+_Finds and stores the titles and publishedAt for the YouTube channels GlobalCyclingNetwork and globalmtb._
+
+**DELETE**
+
+```
+DELETE /youtube/:id
+```
+
+_Finds and deletes a YouTube video's meta data by it's id._
+
 ## Author
 
 #### **Alexander Jack Hughes**
--- routes.js
+++ routes.js
@@ -1,19 +1,39 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-// Middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
-  console.log("Time: ", Date.now());
-  next();
-});
-
-// Define the home page route
+// Helper function
 router.get("/", function(req, res) {
-  res.send("home example");
+  res.send(
+    "Your server is running!\nCheck out the docs for info on how to run the API:\nhttps://github.com/alexjackhughes/youtube-api"
+  );
 });
 
-// Define the about route
-router.get("/about", function(req, res) {
+/**
+ * When hit will store youtube video titles and publishedAt dates
+ * for the GlobalCyclingNetwork and globalmtb YouTube channels
+ * to our database.
+ */
+router.post("/youtube", function(req, res) {
+  res.send("youtube example");
+});
+
+// Fetch any YouTube videos meta data that has been stored.
+router.get("/youtube", function(req, res) {
+  res.send("About example");
+});
+
+// Get a YouTube videos meta data by ID.
+router.get("/youtube/:id", function(req, res) {
+  res.send("About example");
+});
+
+// Search for all videos that match a keyword
+router.get("/youtube/search/:search", function(req, res) {
+  res.send("About example");
+});
+
+// Remove a YouTube videos meta data by ID.
+router.delete("/youtube/:id", function(req, res) {
   res.send("About example");
 });
 
